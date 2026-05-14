0775dcd569e442f858cc5fe31425b87b5c2f7931
Alina Sitkovskaya <alisejene@gmail.com>
2026-05-14 21:23:57 +0300

    Add initial project files including .gitignore, LICENSE, __init__.py, and pyproject.toml

---
+++ lab8/auth-proxy-lib/__init__.py
@@ -0,0 +1,4 @@
+from .src.base_client import BaseClient
+from .src.auth_proxy import ApiKeyAuth, JWTAuth, OAuthProxy
+from .src.logging_proxy import LoggingProxy
+from .src.rate_limit_proxy import RateLimitProxy
